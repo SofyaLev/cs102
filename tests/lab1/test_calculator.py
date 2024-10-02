b4036b828a6841793a2cef78ef7db6bf7affef5d
SofyaLev <sofya_levchuk@bk.ru>
2024-10-02 17:42:22 +0300

    Добавлено решение

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,25 @@
 import unittest
+from cs102.src.lab1.calculator import calculator
+
 
 class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+    def test_addition(self):
+        self.assertEqual(calculator.addition(125, 27), 152)
+        self.assertEqual(calculator.addition(-13, 10), -3)
+        self.assertEqual(calculator.addition(16, 18.5), 34.5)
+
+    def test_subtraction(self):
+        self.assertEqual(calculator.subtraction(125, 23), 102)
+        self.assertEqual(calculator.subtraction(-13, 10), -23)
+        self.assertEqual(calculator.subtraction(16, 18.5), -2.5)
+
+    def test_multiplication(self):
+        self.assertEqual(calculator.multiplication(21, 3), 63)
+        self.assertEqual(calculator.multiplication(-5, 2), -10)
+        self.assertEqual(calculator.multiplication(2.5, 4), 10.0)
+
+    def test_division(self):
+        self.assertEqual(calculator.division(125, 0), 'Error: division by zero')
+        self.assertEqual(calculator.division(-10, 2), -5.0)
+        self.assertEqual(calculator.division(10, 2.5), 4.0)
