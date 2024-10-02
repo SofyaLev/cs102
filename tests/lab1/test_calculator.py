a536548dd81605148cd79c18dbe4711082c03be9
SofyaLev <sofya_levchuk@bk.ru>
2024-10-02 18:40:16 +0300

    Обновление

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Добавляем путь к корневой директории проекта
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 # Импортируем модуль calculator
 from src.lab1.calculator import calculator
